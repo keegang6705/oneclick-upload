86ffe455532f12b3d7f9d250bb1d3b3984140f1b
keegang6705 <75747783+keegang6705@users.noreply.github.com>
2022-06-01 09:12:02 +0700

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,19 @@
+import os,inspect,time
+package_name = "python_package" # your package name
+replaced_name = package_name.replace("_","-") #replace _ - for git hub
+commit = "nothing"
+username = "keegang6705" # your github username
+os.chdir(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+f'/{package_name}_main')
+os.system("git init")
+os.system(f"git remote add {replaced_name} https://github.com/{username}/{replaced_name}")
+os.system("git branch main")
+os.system("git checkout -b main")
+os.system(f"git add {package_name} LICENSE README.md")
+os.system(f"git commit -m \"{commit}\"")
+os.system(f"git push {replaced_name} --force main")
+os.system(f"rmdir /s /q .git")
+os.system("python \""+os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+f"\"/{package_name}/setup.py sdist bdist_wheel")
+os.system("twine upload dist/*")
+print("done")
+os.system(f"rmdir /s /q build dist {package_name}.egg-info")
+print(f"deleted [build,dist,{package_name}.egg-info]")
