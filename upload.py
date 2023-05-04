6c5f36b9e1a327118f789130efdf3fb59aff44e9
keegang6705 <75747783+keegang6705@users.noreply.github.com>
2023-05-04 21:18:09 +0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,10 +1,13 @@
 import os,inspect,time
 commit = "nothing"
-username = "keegang6705" # your github username
+username = "name" # your github username
+useremail= "name@gmail.com" # your github email address
 package_name = "python_package" # your package name
 replaced_name = package_name.replace("_","-") #replace _ - for git hub
 os.chdir(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+f'/{package_name}')
 os.system("git init")
+os.system(f'git config --global "{username}" ')
+os.system(f'git config --global "{useremail}" ')
 os.system(f"git remote add {replaced_name} https://github.com/{username}/{replaced_name}")
 os.system("git branch main")
 os.system("git checkout -b main")
